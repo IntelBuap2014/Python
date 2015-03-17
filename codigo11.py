fcde597dbc9c13666c0a301b92370b3ad876f93d
wozgeass <adrian@aztli.org>
2015-03-17 17:43:50 -0600

    Agregue los primeros 11 scripts

---
+++ codigo11.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+# Lectura de teclado
+numero=input("Ingrese un numero ->\t")
+decimal=input("Ingrese un decimal ->\t")
+cadena=input("Ingrese una cadena ->\t")
+complejo=input("Ingrese un complejo ->\t")
+print ("El tipo de Dato ingresado es [%d]" % numero)
+print ("El tipo de Dato ingresado es [%f]" % decimal)
+print ("El tipo de Dato ingresado es [%s]" % cadena)
+print ("El tipo de Dato ingresado es [%s]" % complejo)
---
+++ codigo12.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+# Lectura de teclado usando eval
+numero=eval(input("Ingrese un numero ->\t"))
+decimal=eval(input("Ingrese un decimal ->\t"))
+cadena=input("Ingrese una cadena ->\t")
+complejo=eval(input("Ingrese un complejo ->\t"))
+print ("El tipo de Dato ingresado es [%d]" % numero)
+print ("El tipo de Dato ingresado es [%f]" % decimal)
+print ("El tipo de Dato ingresado es [%s]" % cadena)
+print ("El tipo de Dato ingresado es [%s]" % complejo)
