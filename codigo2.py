82d8922fd37348503c29919123bd460950d5d972
wozgeass <adrian@aztli.org>
2015-03-17 23:19:30 -0600

    Cambios menores

--- codigo2.py
+++ codigo2.py
@@ -1,2 +1,2 @@
 #! /usr/bin/python3
-print("Hola mundo piñata!")
+print "Hola mundo piñata!"
--- codigo9.py
+++ codigo9.py
@@ -5,7 +5,10 @@ flotante=10.0 # numero decimal
 cadena="Ola Ke ase, pythoneando o ke ase" # una cadena
 Booleano=True # un simple booleano
 complejo=3+2j # numero complejo
-print("Este es un valor entero [ %d ] " % entero)
-print("Este es un valor flotante [ %f ]" % flotante)
+caracter = "a"
+print("Este es un valor entero [ %f ] " % entero)
+print("Este es un valor flotante [ %d ]" % flotante)
 print("Este es un valor Cadena [ %s ]" % cadena)
-print("Este es un valor Booleano [ %d ]" % Booleano)
+print("Este es un valor Booleano [ %s ]" % Booleano)
+print ("Es es un valor complejo [ %s ]" % complejo)
+print ("Caracter expresado en numero es [ %d ]" % ord(caracter))
